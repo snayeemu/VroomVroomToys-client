2ea59e3c06d887734540f8af8e26787a5ad58f53
snayeemu <snayeemu2@gmail.com>
2023-06-15 22:42:22 +0600

    a toy updated

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,3 +1,4 @@
+import { BsStarFill } from "react-icons/bs";
 import { useLoaderData } from "react-router-dom";
 
 const Details = () => {
@@ -12,10 +13,20 @@ const Details = () => {
         <div className="card-body">
           <h2 className="card-title text-xl font-bold mb-4">{toy?.name}</h2>
           <p className="text-gray-300">Seller Name: {toy?.sellerName}</p>
+          <p className="text-gray-300">Seller Email: {toy?.sellerEmail}</p>
           <p className="text-gray-300">Sub-Category: {toy?.subCategory}</p>
           <p className="text-gray-300">Price: {toy?.price}</p>
+
           <p className="text-gray-300">
             Available Quantity: {toy?.availableQuantity}
+          </p>
+          <div className="flex items-center mb-2">
+            {Array.from({ length: Math.floor(toy.rating) }).map((_, index) => (
+              <BsStarFill key={index} className="text-yellow-500 mr-1" />
+            ))}
+          </div>
+          <p className="text-gray-300">
+            <small>Detail Description: {toy?.detailDescription}</small>
           </p>
           <div className="card-actions flex justify-end mt-4">
             {/* <Link to={`/toy/${toy._id}`}>
