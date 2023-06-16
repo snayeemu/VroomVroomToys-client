e17f6ca9c133ea907d58c0d776ff7087f02d7b57
snayeemu <snayeemu2@gmail.com>
2023-06-16 10:25:57 +0600

    redirect after log in

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -10,7 +10,7 @@ const PrivateRoute = ({ children }) => {
   }
   if (user) return children;
 
-  return <Navigate to={"/"} state={{ from: location }} replace></Navigate>;
+  return <Navigate to={"/login"} state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
